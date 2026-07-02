1d515caca6a46f9a37c6e73a254db0f4066b5375
GonzaloRomero66 <estudio.romero66@gmail.com>
2026-07-02 19:30:08 -0300

    se corrieron muchos errores de la pagina, como que se intentaba mostrar algo cuando se actualizaba y de mas

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,8 @@ const CDN_URL = "https://cdn.thesimpsonsapi.com/500";
 const contenedor = document.getElementById("cartaPj");
 const mensaje = document.getElementById("mensajeEstado");
 const inputBuscar = document.getElementById("buscarPj");
+const formBuscar = document.getElementById("formBuscar");
+const btnLimpiar = document.getElementById("btnLimpiar");
 let personajes = [];
 
 async function obtenerPersonajes() {
@@ -16,10 +18,10 @@ async function obtenerPersonajes() {
         mensaje.innerHTML = `<div class="alert alert-danger">No se pudieron cargar los personajes</div>`;
     }
 }
-function mostrarPersonajes(lista){
-    contenedor.innerHTML = "";
-    if(lista.length === 0) {
-        mensaje.innerHTML = `<div class="alert alert-warning">No se encontraron personajes</div>`;
+function mostrarPersonajes(lista) {
+  contenedor.innerHTML = "";
+  if (lista.length === 0) {
+    mensaje.innerHTML = `<div class="alert alert-warning">No se encontraron personajes</div>`;
          return;
     }
     mensaje.innerHTML = "";
@@ -42,45 +44,48 @@ function mostrarPersonajes(lista){
         `;
     });
 }
-function filtrarPersonajes () {
-    const texto = inputBuscar.value.trim().toLowerCase();
-    if (texto === "") {
-        mostrarPersonajes(personajes);
-        return;
-    }  const filtrados = personajes.filter((personaje) => 
-
-        personaje.name.toLowerCase().includes(texto)
-    );
-    mostrarPersonajes(filtrados);
+function filtrarPersonajes() {
+  const texto = inputBuscar.value.trim().toLowerCase();
+  if (texto === "") {
+    mostrarPersonajes(personajes);
+    return;
+  }
+  const filtrados = personajes.filter((personaje) =>
+    personaje.name.toLowerCase().includes(texto),
+  );
+  mostrarPersonajes(filtrados);
 }
 obtenerPersonajes()
 inputBuscar.addEventListener("input", filtrarPersonajes);
 
 async function obtenerDetalle(id) {
     try {
-       const respuesta = await fetch(`${API_URL}/${id}`);
-       const personaje = await respuesta.json()
-
+    const respuesta = await fetch(`${API_URL}/${id}`);
+     const personaje = await respuesta.json();
         mostrarModal(personaje);
    } catch (error){
         console.log(error);
         alert("No se pudo cargar el detalle del personaje");
     }
 }
-
-function mostrarModal(personaje){
-   document.getElementById("modalNombre").textContent = personaje.name;
-    document.getElementById("modalImagen").src = CDN_URL + personaje.portrait_path;
-   document.getElementById("modalEdad").textContent = personaje.age ?? "Desconocida";
-    document.getElementById("modalNacimiento").textContent = personaje.birthdate ?? "Desconocida";
-    document.getElementById("modalGenero").textContent = personaje.occupation;
-    document.getElementById("modalOcupacion").textContent = personaje.occupation;
-    document.getElementById("modalEstado").textContent = personaje.status;
-   document.getElementById("modalFrase").textContent = personaje.phrases[0] ?? "Sin frases";
-
-   const modal = new bootstrap.Modal(document.getElementById("modalDetalle"));
-    modal.show();
+function mostrarModal(personaje) {
+  document.getElementById("modalNombre").textContent = personaje.name;
+  document.getElementById("modalImagen").src =
+    CDN_URL + personaje.portrait_path;
+   document.getElementById("modalEdad").textContent =
+    personaje.age ?? "Desconocida";
+  document.getElementById("modalNacimiento").textContent =
+    personaje.birthdate ?? "Desconocida";
+  document.getElementById("modalGenero").textContent =
+    personaje.gender ?? "Desconocido";
+  document.getElementById("modalOcupacion").textContent = personaje.occupation;
+  document.getElementById("modalEstado").textContent = personaje.status;
+  document.getElementById("modalFrase").textContent =
+    personaje.phrases[0] ?? "Sin frases";
+    const modal = new bootstrap.Modal(document.getElementById("modalDetalle"));
+  modal.show();
 }
+  
 
 document.addEventListener("DOMContentLoaded", obtenerPersonajes);
 inputBuscar.addEventListener("input", filtrarPersonajes);
@@ -89,4 +94,9 @@ contenedor.addEventListener("click", (event) => {
     const id = event.target.dataset.id;
     obtenerDetalle(id);
   }
+});
+formBuscar.addEventListener("submit", (event) => {
+  event.preventDefault();
+  inputBuscar.value = "";
+  mostrarPersonajes(personajes);
 });
